68806946c0046fa42a68b7605c7b43c9e6143e8e
bacnx <bacnx@outfiz.com>
2025-03-16 23:22:32 +0700

    done13.1

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,40 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { useState } from 'react'
+import { useEffect } from 'react'
+
+const wellCome = [ " Xin chào", "Hello", "Bonjour", "Hola", "Ciao", "Hallo", "こんにちは", "안녕하세요", "สวัสดี","Hi" ]
+
+function Header() {
+  const [time, setTime] = useState(new Date().toLocaleTimeString())
+  const [date, setDate] = useState(new Date().toLocaleDateString())
+  const [greeting, setGreating] = useState(wellCome[Math.floor(Math.random()*10)])
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setTime(new Date().toLocaleTimeString())
+      setDate(new Date().toLocaleDateString())
+    }, 1000)
+    return () => clearInterval(interval)
+  }, [])
+  useEffect(() => {
+    const interval1 = setInterval(() => {
+      setGreating(wellCome[Math.floor(Math.random()*10)])
+    }, 3000)
+    return () => clearInterval(interval1)
+  }, [])
+  return (
+    <>
+      <h1>{greeting}</h1>
+      <p>Hôm nay là: <strong> {date} </strong>, Thời gian hiện tại: 
+      <strong>{time}</strong></p>
+    </>
+  )
+}
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+     <Header />
     </>
   )
 }
